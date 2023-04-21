62807a442337754ead30700776cfc1821353c230
black script <111915039+blackscriptt@users.noreply.github.com>
2023-04-21 22:39:29 +0300

    Update app.js

--- homeworks/test-05/js/app.js
+++ homeworks/test-05/js/app.js
@@ -17,7 +17,34 @@ $(document).ready(function() {
             .delay(100)
             .animate({ opacity: '1' });
          }
-        
     });
 
+
+    let batterys = [
+        "fa-battery-full",
+        "fa-battery-three-quarters",
+        "fa-battery-half",
+        "fa-battery-quarter",
+        "fa-battery-empty"
+    ];
+    let counter = 4;
+
+
+    setInterval(() => {
+        const date = new Date().toLocaleTimeString();
+        let time = date.split(':');
+        $('.clock').text(`${time[0]}.${time[1]}`);
+
+
+        if(counter >= 0) {
+            batterys.forEach(item => {
+                $('.battery i').removeClass(item);
+            });
+
+            $('.battery i').addClass(batterys[counter]);
+            counter--;
+        } else {
+            counter= 4;
+        }
+     }, 1000);
 });
